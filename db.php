882ed98267b92360d6ee4deef5fb530f9f0600b5
stanis <stanis@netvolution.fr>
2023-01-30 14:49:55 +0100

    Ajout fichier db.php

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+require 'config.php';
+$dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET;
+
+try {
+    $db = new PDO($dsn, DB_USER, DB_PASS);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
